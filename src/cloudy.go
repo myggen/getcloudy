1ffee1e27fb84ce2236a9fb995097f3f1b8dff01
Espen Myrland <espen@toycompute.net>
2020-04-22 12:14:54 +0200

    Added reftime query parameter

--- src/cloudy.go
+++ src/cloudy.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
+	"time"
 )
 
 func main() {
@@ -24,10 +25,42 @@ func main() {
 
 }
 func predictions(w http.ResponseWriter, r *http.Request) {
-	bytes, err := ioutil.ReadFile("/lustre/store/project/metproduction/products/camrec/predictions.json")
+
+	// Current reftime
+	path := "/lustre/store/project/metproduction/products/camrec/predictions.json"
+
+	reftimes, ok := r.URL.Query()["reftime"]
+
+	if !ok || len(reftimes[0]) < 1 {
+		log.Println("Url Param 'reftime' given. Using current")
+
+	} else {
+		reftimeSstr := reftimes[0]
+		reftime, err := time.Parse("20060102T1504Z", reftimeSstr)
+		if err != nil {
+			log.Printf("Error parsing reftime:  %v", err)
+			w.WriteHeader(400)
+			return
+		}
+		log.Printf("Reftime: %v", reftime)
+		datadir := "/lustre/store/project/metproduction/products/camrec"
+		path = fmt.Sprintf("%s/%s/%s/%s/predictions_%s.json", datadir,
+			reftime.Format("2006"),
+			reftime.Format("01"),
+			reftime.Format("02"),
+			reftime.Format("20060102T1504"),
+		)
+
+		log.Printf("PAth: %s", path)
+	}
+
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+
+	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		http.Error(w, err.Error(),
-				http.StatusInternalServerError)
+			http.StatusInternalServerError)
 	}
 	fmt.Fprintf(w, string(bytes))
 }
